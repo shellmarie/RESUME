bc917581994a2cb77f02e8d2d0b2a221b37d71da
shellmarie <155805233+shellmarie@users.noreply.github.com>
2024-02-24 16:51:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('colorButton').addEventListener('click', function() {
+    document.body.style.backgroundColor = '#FFE7C1';
+    document.body.style.color = '#555';
+    });
+    
